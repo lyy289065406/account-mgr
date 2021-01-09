3dbf0f49e047b5d7a2576bbdfe876e3980fb64db
EXP <289065406@qq.com>
2021-01-10 00:29:12 +0800

    init

--- ruoyi/bin/run.sh
+++ ruoyi/bin/run.sh
@@ -2,4 +2,4 @@ echo "[INFO] run the application."
 source /etc/profile
 cd /opt/ruoyi
 
-java -jar -Xms256m -Xmx1024m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=512m ruoyi-admin/target/ruoyi-admin.jar &
+java -jar -Xms256m -Xmx1024m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=512m ruoyi-admin/target/ruoyi-admin.jar > /dev/null 2>&1 &
