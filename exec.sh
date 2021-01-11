05d0648e9c381acec862118e1a00d260dc698aed
exp.liao@dji.com <exp.liao@dji.com>
2021-01-11 19:08:22 +0800

    执行脚本优化

--- exec.sh
+++ exec.sh
@@ -21,32 +21,32 @@ function help()
 if [ -z "${script_name}" ] ; then
     help
 
-elif [ "$script_name"x -eq "up"x ] ; then
+elif [ "$script_name"x = "up"x ] ; then
     docker-compose up -d
 
-elif [ "$script_name"x -eq "init"x ] || [ "$script_name"x -eq "db"x ] ; then
+elif [ "$script_name"x = "init"x ] || [ "$script_name"x = "db"x ] ; then
     docker exec -u mysql ACCOUNT_MGR /bin/bash -c "mysql -uroot -p123456 -e 'source /var/sql/init.sql'"
     docker exec -u mysql ACCOUNT_MGR /bin/bash -c "mysql -uroot -p123456 -e 'source /opt/ruoyi/sql/ry_20201214.sql'"
     docker exec -u mysql ACCOUNT_MGR /bin/bash -c "mysql -uroot -p123456 -e 'source /opt/ruoyi/sql/quartz.sql'"
     docker exec -u mysql ACCOUNT_MGR /bin/bash -c "mysql -uroot -p123456 -e 'source /opt/ruoyi/sql/am_backend.sql'"
     docker exec -u mysql ACCOUNT_MGR /bin/bash -c "mysql -uroot -p123456 -e 'source /opt/ruoyi/sql/am_frontend.sql'"
 
-elif [ "$script_name"x -eq "cln"x ] || [ "$script_name"x -eq "clean"x ] ; then
+elif [ "$script_name"x = "cln"x ] || [ "$script_name"x = "clean"x ] ; then
     docker exec -u root ACCOUNT_MGR /bin/bash -c "/run/ruoyi/bin/clean.sh"
 
-elif [ "$script_name"x -eq "pkg"x ] || [ "$script_name"x -eq "package"x ] ; then
+elif [ "$script_name"x = "pkg"x ] || [ "$script_name"x = "package"x ] ; then
     docker exec -u root ACCOUNT_MGR /bin/bash -c "/run/ruoyi/bin/package.sh"
 
-elif [ "$script_name"x -eq "run"x ] || [ "$script_name"x -eq "start"x ] ; then
+elif [ "$script_name"x = "run"x ] || [ "$script_name"x = "start"x ] ; then
     docker exec -u root ACCOUNT_MGR /bin/bash -c "/run/ruoyi/bin/run.sh"
 
-elif [ "$script_name"x -eq "stop"x ] ; then
+elif [ "$script_name"x = "stop"x ] ; then
     docker exec -u root ACCOUNT_MGR /bin/bash -c "/run/ruoyi/bin/stop.sh"
 
-elif [ "$script_name"x -eq "down"x ] ; then
+elif [ "$script_name"x = "down"x ] ; then
     docker-compose down
 
-elif [ "$script_name"x -eq "shell"x ] || [ "$script_name"x -eq "bash"x ] ; then
+elif [ "$script_name"x = "shell"x ] || [ "$script_name"x = "bash"x ] ; then
     docker exec -it -u root ACCOUNT_MGR /bin/bash
 
 else
